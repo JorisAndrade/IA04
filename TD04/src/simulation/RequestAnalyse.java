2c9f414ea6750bcbf29788d308c7b31465e95dd4
Joris Andrade <joris.andrade@gmail.com>
2014-03-31 18:37:39 +0200

    Add missing class

---
+++ TD04/src/simulation/RequestAnalyse.java
@@ -0,0 +1,47 @@
+package simulation;
+
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.io.IOException;
+
+import utils.CellsMessage;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SuppressWarnings("serial")
+public class RequestAnalyse extends CyclicBehaviour {
+
+	private final ObjectMapper mapper = new ObjectMapper();
+
+	@Override
+	public void action() {
+		MessageTemplate template = MessageTemplate.and(
+				MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+				MessageTemplate.MatchConversationId("1"));
+		ACLMessage response = myAgent.receive(template);
+		System.out.println("Wait for message");
+		if (response != null) {
+			ACLMessage message = new ACLMessage(ACLMessage.REQUEST);
+
+			CellsMessage requestMessage;
+			try {
+				requestMessage = mapper.readValue(response.getContent(),
+						CellsMessage.class);
+				int rank = requestMessage.getRank();
+				message.setContent(response.getContent());
+				message.addReceiver(((SimulationAgent) myAgent).agents[rank]);
+				System.out.println("Sending : " + response.getContent()
+						+ "to agent " + rank);
+				myAgent.send(message);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else {
+			block();
+		}
+	}
+}
